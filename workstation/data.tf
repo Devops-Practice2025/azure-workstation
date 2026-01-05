c07e6f09fdb6f21884fd3bdebe3034a5f208f2d6
karthikeyan-g16 <karthikeyangopal16@gmail.com>
2026-01-05 14:57:32 +0100

    Update data.tf

--- workstation/data.tf
+++ workstation/data.tf
@@ -1,7 +1,7 @@
 data "azurerm_subnet" "example" {
   name                 = "default"
   virtual_network_name = "test-vnet"
-  resource_group_name  = var.main.resource_group_name
+  resource_group_name = "nebula-sandbox-karthikeyangopal-2a6a0756"
 }
 
 output "subnet_id" {
