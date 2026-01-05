8d4fce4f362c9cbcaae3580beb786a07524d5819
karthikeyan-g16 <karthikeyangopal16@gmail.com>
2026-01-05 10:34:45 +0100

    Update and rename data.tf to variable.tf

---
+++ workstation/variable.tf
@@ -0,0 +1,6 @@
+variable {
+  resource_group_name = "nebula-sandbox-karthikeyangopal-2a6a0756"
+  location = "eastus"
+  subnet_name                 = "default"
+  virtual_network_name = "test-vnet"
+}
