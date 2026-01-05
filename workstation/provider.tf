db39ab3c6d0f2c6f54d9f4672ba85c3637b53f46
karthikeyan-g16 <karthikeyangopal16@gmail.com>
2026-01-05 10:21:42 +0100

    Update provider.tf

--- workstation/provider.tf
+++ workstation/provider.tf
@@ -3,6 +3,7 @@ provider "azurerm" {
   subscription_id = "5556f231-83ae-4b43-852a-5bacdb7558b6"
   #skip_provider_registration = true
   resource_provider_registrations = "none"
+  location = "eastus"
 
 
 }
