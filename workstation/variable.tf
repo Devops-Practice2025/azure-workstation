2ef2b5c12b5d0f69a08cdf7ea5c3fd67ee6a789b
karthikeyan-g16 <karthikeyangopal16@gmail.com>
2026-01-05 14:51:59 +0100

    Update variable.tf

--- workstation/variable.tf
+++ workstation/variable.tf
@@ -1,6 +1,4 @@
 variable "main" {
   resource_group_name = "nebula-sandbox-karthikeyangopal-2a6a0756"
   location = "eastus"
-  subnet_name                 = "default"
-  virtual_network_name = "test-vnet"
 }
