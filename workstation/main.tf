7bf75ed63d51bec494206e1e04a429d2c9294393
karthikeyan-g16 <karthikeyangopal16@gmail.com>
2026-01-05 14:54:37 +0100

    Update main.tf

--- workstation/main.tf
+++ workstation/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_network_interface" "example" {
   name                = "test-nic"
-  location            = var.main.location
+  #location            = var.main.location
   resource_group_name = var.main.resoruce_group_name
 
   ip_configuration {
@@ -13,8 +13,8 @@ resource "azurerm_network_interface" "example" {
 
 resource "azurerm_virtual_machine" "main" {
   name                  = "test-vm"
-  location              = var.main.location
-  resource_group_name   = var.main.name
+  #location              = var.main.location
+  resource_group_name   = var.main.resoruce_group_name
   network_interface_ids = [azurerm_network_interface.example.id]
   vm_size               = "Standard_DS1_v2"
 
