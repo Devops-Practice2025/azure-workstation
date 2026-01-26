35f9123085c046dfef1f22b580a3d845c741bcc5
karthikeyan <karthikeyangopal16@gmail.com>
2026-01-26 10:06:33 +0100

    test

--- az-cli.sh
+++ az-cli.sh
@@ -1,7 +1,7 @@
 set -x
 
-sub_id = $(az account list -o table | grep C_029773-training-subscription | awk '{print $3}')
-rg = $(az group list -o table | grep nebula | awk '{print $1}')
+sub_id=$(az account list -o table | grep C_029773-training-subscription | awk '{print $3}')
+rg=$(az group list -o table | grep nebula | awk '{print $1}')
 read -p "Enter your pass for VM workstation: " ps
 
 az account set --subscription $sub_id
