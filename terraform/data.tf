4bd6b1cad997df4aa58041729f2e3bd617415856
karthikeyan <karthikeyangopal16@gmail.com>
2026-01-26 17:02:36 +0100

    test

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,6 +1,6 @@
 # Data block to read an existing resource group
 data "azurerm_resource_group" "nebula" {
-     name = "nebula-sandbox-karthikeyangopal-629394fa"
+     name = "nebula-sandbox-karthikeyangopal-6bc4873c"
 }
 locals {
   nebula_rg = data.azurerm_resource_group.nebula.name
