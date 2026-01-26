1165a141eb9f72659cc68d0d3b81198618b45113
karthikeyan <karthikeyangopal16@gmail.com>
2026-01-26 16:53:10 +0100

    test

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,6 +1,6 @@
 # Data block to read an existing resource group
 data "azurerm_resource_group" "nebula" {
-    name_prefix = "nebula"
+     name_prefix = "nebula"
 }
 locals {
   nebula_rg = data.azurerm_resource_group.nebula.name[0]
