00986b9d545a202164d72b4714afd2d84826c71e
karthikeyan <karthikeyangopal16@gmail.com>
2026-02-11 16:20:21 +0100

    test

--- aks/providers.tf
+++ aks/providers.tf
@@ -15,7 +15,7 @@ terraform {
   backend "azurerm" {
     use_cli              = true                                    # Can also be set via `ARM_USE_CLI` environment variable.
     use_azuread_auth     = true                                    # Can also be set via `ARM_USE_AZUREAD` environment variable.
-    storage_account_name = "store_2026"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    storage_account_name = "store202602"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "tfstate"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "dev.terraform.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
