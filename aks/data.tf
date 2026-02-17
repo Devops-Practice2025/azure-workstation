f113148d9b19dfb1413833fb1d8f1195ca20f74e
karthikeyan <karthikeyangopal16@gmail.com>
2026-02-17 11:00:44 +0100

    test

--- aks/data.tf
+++ aks/data.tf
@@ -1,10 +1,12 @@
-data "azurerm_resource_groups" "aks-rg" {}
+data "azurerm_resource_groups" "aks-rg" {
+
+}
 
 locals {
   # 2. Filter the list using a for loop and regex
   # This matches names starting with 'openenv' OR 'nebula'
   filtered_groups = [
-    for rg in data.azurerm_resource_groups.all.resource_groups : rg.name 
+    for rg in data.azurerm_resource_groups.aks-rg.resource_groups : rg.name 
     if can(regex("^(openenv|nebula)", rg.name))
   ]
 }
