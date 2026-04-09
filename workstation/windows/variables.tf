0ea97f9f0d1d4296b943487ab47e6dfe6aa05a99
karthikeyan <karthikeyangopal16@gmail.com>
2026-04-09 08:37:03 +0200

    test

---
+++ workstation/windows/variables.tf
@@ -0,0 +1,31 @@
+variable "location" {
+  default = "westus"
+}
+
+variable "vm_admin_username" {
+  default = "azureuser"
+}
+
+variable "vm_admin_password" {
+  description = "Windows admin password"
+  sensitive   = true
+}
+
+output "public_ip" {
+  value = azurerm_public_ip.pip.ip_address
+}
+
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.100"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
---
+++ workstation/windows/vm.tf
@@ -0,0 +1,37 @@
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                = "testwin-clean"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_D2_v2"
+  admin_username      = var.vm_admin_username
+  admin_password      = var.vm_admin_password
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    disk_size_gb         = 127
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-datacenter"
+    version   = "latest"
+  }
+}
+resource "azurerm_virtual_machine_extension" "chrome" {
+  name                 = "install-chrome"
+  virtual_machine_id   = azurerm_windows_virtual_machine.vm.id
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.10"
+
+  settings = <<SETTINGS
+{
+  "commandToExecute": "powershell -ExecutionPolicy Bypass -Command \"Invoke-WebRequest -Uri https://dl.google.com/chrome/install/latest/chrome_installer.exe -OutFile C:\\\\chrome.exe; Start-Process C:\\\\chrome.exe -ArgumentList '/silent /install' -Wait\""
+}
+SETTINGS
+}
