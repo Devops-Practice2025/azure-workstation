afa19624dc7857ac077a99a57bf3bae3d9fcae96
karthikeyan <karthikeyangopal16@gmail.com>
2026-04-10 11:04:55 +0200

    test

--- workstation/windows/install-sshd.ps1
+++ workstation/windows/install-sshd.ps1
@@ -1,19 +1,29 @@
 #############################################
-# INSTALL GOOGLE CHROME
+# TRY INSTALL GOOGLE CHROME (NON-BLOCKING)
 #############################################
 
-$chromeInstaller = "$env:TEMP\chrome.msi"
+try {
 
-Invoke-WebRequest `
-"https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B00000000-0000-0000-0000-000000000000%7D/dl/chrome/install/googlechromestandaloneenterprise64.msi" `
--OutFile $chromeInstaller
+    $chromeInstaller = "$env:TEMP\chrome.msi"
 
-Start-Process msiexec.exe `
--ArgumentList "/i $chromeInstaller /qn" `
--Wait
+    Invoke-WebRequest `
+    "https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B00000000-0000-0000-0000-000000000000%7D/dl/chrome/install/googlechromestandaloneenterprise64.msi" `
+    -OutFile $chromeInstaller `
+    -ErrorAction Stop
+
+    Start-Process msiexec.exe `
+    -ArgumentList "/i $chromeInstaller /qn" `
+    -Wait
+
+    Write-Output "Chrome Installed Successfully"
+
+}
+catch {
+    Write-Output "Chrome install skipped: No outbound internet"
+}
 
 #############################################
-# INSTALL OPENSSH SERVER
+# INSTALL OPENSSH SERVER (MANDATORY)
 #############################################
 
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
@@ -32,7 +42,8 @@ New-NetFirewallRule `
 -Direction Inbound `
 -Protocol TCP `
 -Action Allow `
--LocalPort 443
+-LocalPort 443 `
+-ErrorAction SilentlyContinue
 
 $configPath = "C:\ProgramData\ssh\sshd_config"
 
@@ -44,4 +55,5 @@ Add-Content $configPath "AllowTcpForwarding yes"
 Add-Content $configPath "GatewayPorts no"
 
 Restart-Service sshd
-``
+
+exit 0
