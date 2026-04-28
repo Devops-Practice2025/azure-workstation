dc26bd32b4f006b13973fa7b70f18d7d36da21ea
karthikeyan <karthikeyangopal16@gmail.com>
2026-04-28 17:21:24 +0200

    test

--- workstation/linux/main.tf
+++ workstation/linux/main.tf
@@ -87,6 +87,17 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
