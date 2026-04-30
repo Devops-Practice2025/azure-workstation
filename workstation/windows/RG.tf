85732d360cc3f0a06d1df9ba9f31723b93dd9c32
karthikeyan <karthikeyangopal16@gmail.com>
2026-04-30 15:18:07 +0200

    test

--- workstation/windows/RG.tf
+++ workstation/windows/RG.tf
@@ -7,19 +7,19 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "vm-vnet"
   address_space       = ["10.50.0.0/16"]
   location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = data.azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
   name                 = "vm-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.50.1.0/24"]
 }
 resource "azurerm_network_security_group" "nsg" {
   name                = "vm-nsg"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  resource_group_name = data.azurerm_resource_group.rg.name
 
   security_rule {
     name                       = "Allow-RDP"
@@ -36,7 +36,7 @@ resource "azurerm_network_security_group" "nsg" {
 resource "azurerm_public_ip" "pip" {
   name                = "vm-pip"
   location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = data.azurerm_resource_group.rg.name
   allocation_method   = "Static"
   sku                 = "Standard"
 }
@@ -44,7 +44,7 @@ resource "azurerm_public_ip" "pip" {
 resource "azurerm_network_interface" "nic" {
   name                = "vm-nic"
   location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = data.azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "ipconfig1"
@@ -69,6 +69,6 @@ resource "azurerm_network_security_rule" "allow_ssh443" {
   destination_port_range      = "443"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.rg.name
+  resource_group_name         = data.azurerm_resource_group.rg.name
   network_security_group_name = azurerm_network_security_group.nsg.name
 }
