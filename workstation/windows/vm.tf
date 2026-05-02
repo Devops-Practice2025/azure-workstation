637df84a8c365b1239c05b0fd00086c3e09fc11f
karthikeyan <karthikeyangopal16@gmail.com>
2026-05-02 09:40:52 +0200

    test

--- workstation/windows/vm.tf
+++ workstation/windows/vm.tf
@@ -2,7 +2,7 @@ resource "azurerm_windows_virtual_machine" "vm" {
   name                = "testwin2"
   resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
-  size                = "Standard_D2s_v3" 
+  size                = "Standard_D2ds_v5" 
   admin_username      = var.vm_admin_username
   admin_password      = var.vm_admin_password
   network_interface_ids = [
