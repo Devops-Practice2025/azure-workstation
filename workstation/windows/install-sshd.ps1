fe91a281c4adb0044e4a0d9ef5032e4fc86737e7
karthikeyan <karthikeyangopal16@gmail.com>
2026-05-02 08:22:57 +0200

    test

--- workstation/windows/install-sshd.ps1
+++ workstation/windows/install-sshd.ps1
@@ -1,43 +1,4 @@
-# #############################################
-# # TRY INSTALL GOOGLE CHROME (NON-BLOCKING)
-# #############################################
 
-# try {
-#     # Force TLS 1.2 (REQUIRED for Google download)
-#     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-#     $chromeInstaller = Join-Path $env:TEMP "chrome.msi"
-
-#     Write-Output "Downloading Chrome MSI..."
-
-#     Invoke-WebRequest `
-#         -Uri "https://dl.google.com/dl/chrome/install/googlechromestandaloneenterprise64.msi" `
-#         -OutFile $chromeInstaller `
-#         -UseBasicParsing `
-#         -ErrorAction Stop
-
-#     Write-Output "Installing Chrome..."
-
-#     $arguments = "/i `"$chromeInstaller`" /qn /norestart"
-
-#     $process = Start-Process `
-#         -FilePath "msiexec.exe" `
-#         -ArgumentList $arguments `
-#         -Wait `
-#         -PassThru `
-#         -ErrorAction Stop
-
-#     if ($process.ExitCode -eq 0) {
-#         Write-Output "✅ Chrome installed successfully"
-#     }
-#     else {
-#         Write-Output "⚠️ Chrome installer exited with code $($process.ExitCode)"
-#     }
-# }
-# catch {
-#     Write-Output "❌ Chrome installation failed:"
-#     Write-Output $_.Exception.Message
-# }
 
 
 #############################################
@@ -49,9 +10,9 @@ Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 Start-Service sshd
 Set-Service -Name sshd -StartupType Automatic
 
-#############################################
-# MOVE SSH TO 443
-#############################################
+############################################
+#MOVE SSH TO 443
+############################################
 
 New-NetFirewallRule `
 -Name ssh443 `
--- workstation/windows/vm.tf
+++ workstation/windows/vm.tf
@@ -1,6 +1,6 @@
 resource "azurerm_windows_virtual_machine" "vm" {
   name                = "testwin2"
-  resource_group_name = data.azurerm_resource_group.rg.name
+  resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
   size                = "Standard_D2s_v3" 
   admin_username      = var.vm_admin_username
@@ -26,23 +26,23 @@ source_image_reference {
 }
 
 
-resource "azurerm_virtual_machine_extension" "install_sshd" {
+# resource "azurerm_virtual_machine_extension" "install_sshd" {
 
-  name                 = "install-openssh"
-  virtual_machine_id   = azurerm_windows_virtual_machine.vm.id
-  publisher            = "Microsoft.Compute"
-  type                 = "CustomScriptExtension"
-  type_handler_version = "1.10"
+#   name                 = "install-openssh"
+#   virtual_machine_id   = azurerm_windows_virtual_machine.vm.id
+#   publisher            = "Microsoft.Compute"
+#   type                 = "CustomScriptExtension"
+#   type_handler_version = "1.10"
 
-  protected_settings = jsonencode({
-    commandToExecute = "powershell -ExecutionPolicy Bypass -EncodedCommand ${local.sshd_script}"
-  })
-}
+#   protected_settings = jsonencode({
+#     commandToExecute = "powershell -ExecutionPolicy Bypass -EncodedCommand ${local.sshd_script}"
+#   })
+# }
 
-locals {
-  sshd_script = textencodebase64(
-    file("${path.module}/install-sshd.ps1"),
-    "UTF-16LE"
-  )
-}
+# locals {
+#   sshd_script = textencodebase64(
+#     file("${path.module}/install-sshd.ps1"),
+#     "UTF-16LE"
+#   )
+# }
 
