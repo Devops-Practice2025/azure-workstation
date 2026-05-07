07fcfc57fdf549bf3d529110de0af908a53d47da
karthikeyan <karthikeyangopal16@gmail.com>
2026-05-07 08:25:56 +0200

    test

--- workstation/windows/vm.tf
+++ workstation/windows/vm.tf
@@ -1,5 +1,5 @@
 resource "azurerm_windows_virtual_machine" "vm" {
-  name                = "testwin2"
+  name                = "testwin1"
   resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
   size                = "Standard_D2ds_v5" 
